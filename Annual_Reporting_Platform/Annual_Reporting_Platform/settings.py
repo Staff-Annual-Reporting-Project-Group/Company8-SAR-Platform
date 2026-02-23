b27c37033a5ee1b5b04407f4c6edd8bb8341b334
jjdlabee <jjdlabee@gmail.com>
2026-02-23 14:32:34 -0400

    Updates to deploy on Render

--- Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
+++ Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-nn%zl$p377&4y
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['https://company8-sar-platform.onrender.com/', 'your-custom-domain.com']
-if DEBUG:
-    ALLOWED_HOSTS.append('localhost')
-    ALLOWED_HOSTS.append('127.0.0.1')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
 
