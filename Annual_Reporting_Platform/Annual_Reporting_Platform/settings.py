9ac976015e3a0cb5413bebbde62efcf1ce54c55a
jjdlabee <jjdlabee@gmail.com>
2026-02-24 00:40:10 -0400

    fixed site url problem

--- Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
+++ Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
@@ -126,6 +126,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-SITE_URL = os.environ.get('SITE_URL', 'http://127.0.0.1:8000')
-
 DEFAULT_FROM_EMAIL = 'DCIT Reports <noreply@example.com>'
