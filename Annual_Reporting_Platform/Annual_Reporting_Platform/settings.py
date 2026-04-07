7cf49ba24ba07337b9dbffa446728cc8904aea9c
Joshua Weekes <jweekes.business@gmail.com>
2026-04-07 09:28:35 -0400

    Admin App

--- Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
+++ Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reports.apps.ReportsConfig',
     'users.apps.UsersConfig',
+    'administration.apps.AdministrationConfig',
 ]
 
 MIDDLEWARE = [
