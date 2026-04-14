cd44fd2fb2e61f49679b6d98f1fa6d865d1f881d
jjdlabee <jjdlabee@gmail.com>
2026-04-14 11:53:17 -0400

    Connected postgres DB via Neon

--- Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
+++ Annual_Reporting_Platform/Annual_Reporting_Platform/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -85,8 +86,10 @@ WSGI_APPLICATION = 'Annual_Reporting_Platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 DATABASES = {
-    'default': {
+    'default': dj_database_url.parse(DATABASE_URL) if DATABASE_URL else {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
